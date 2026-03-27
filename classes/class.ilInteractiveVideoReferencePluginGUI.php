fbeb15a612f948b42e81611d568a8d2e6527434a
Guido Vollbach <gvollbach@databay.de>
2026-03-27 10:16:47 +0100

    Fix css

--- classes/class.ilInteractiveVideoReferencePluginGUI.php
+++ classes/class.ilInteractiveVideoReferencePluginGUI.php
@@ -337,7 +337,7 @@ class ilInteractiveVideoReferencePluginGUI extends \ilPageComponentPluginGUI
                 $tpl->setVariable('LINKED_TITLE', $xvid->getTitle());
                 $tpl->setVariable('URL', ilLink::_getLink($ref_id, 'xvid', $params));
             } else {
-                $tpl->setVariable('UNLINKED_TITLE', '<h2>' . $xvid->getTitle() . '</h2>');
+                $tpl->setVariable('UNLINKED_TITLE', '<h2> <img src="'. ilObject::_getIcon($xvid->getId()) . '" alt="'. $xvid->getTitle() . '"/> ' . $xvid->getTitle() . '</h2>');
             }
             return $tpl->get();
         }
