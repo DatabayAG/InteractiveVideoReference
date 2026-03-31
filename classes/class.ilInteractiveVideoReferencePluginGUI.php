94484f7bfd1a00fa2fb98a730b0caaac83f68b26
Guido Vollbach <gvollbach@databay.de>
2026-03-31 09:18:24 +0200

    Remove empty template block

--- classes/class.ilInteractiveVideoReferencePluginGUI.php
+++ classes/class.ilInteractiveVideoReferencePluginGUI.php
@@ -288,7 +288,7 @@ class ilInteractiveVideoReferencePluginGUI extends \ilPageComponentPluginGUI
 
         /** @var ilInteractiveVideoReferencePlugin $pl */
         $pl  = $this->getLocalPlugin();
-        $tpl = $this->getPlugin()->getTemplate('tpl.content.html', false, false);
+        $tpl = $this->getPlugin()->getTemplate('tpl.content.html', false, true);
         $question = new ilTemplate("tpl.simple_questions.html", true, true, ilInteractiveVideoPlugin::getInstance()->getDirectory());
 
         $GLOBALS['tpl']->addCss('./Customizing/global/plugins/Services/COPage/PageComponent/InteractiveVideoReference/templates/xvid_ref.css');
